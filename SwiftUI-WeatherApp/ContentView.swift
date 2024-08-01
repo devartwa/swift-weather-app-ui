5f28779de62e4be2ff2f5b6ab8623d97b38d2d8d
devartwa <arthurbuenoneto@gmail.com>
2024-08-01 11:38:21 +1000

    Initial Commit

---
+++ SwiftUI-WeatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-WeatherApp
+//
+//  Created by Arthur Bueno on 01/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-WeatherApp/SwiftUI_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherAppApp.swift
+//  SwiftUI-WeatherApp
+//
+//  Created by Arthur Bueno on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
